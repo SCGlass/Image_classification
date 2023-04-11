db87cbc4d99dbb0b85b4d469b48ab1a6217e86df
Sam Glass <sam.charles.glass@gmail.com>
2023-04-11 21:52:17 +0200

    re installing matplot lib

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from tensorflow.keras import datasets, layers, models
 # importing the training and testing from dataset in tensorflow. Saving them as tuples. It will return them as arrays as pixels 
 (training_images, training_labels), (testing_images, testing_labels) = datasets.cifar10.load_data()
 # we now need to scale the pixels from value of 0 to 255 to 0 to 1, Will normalize it. 
-training_images,training_images = training_images  / 255, testing_images / 255
+training_images, testing_images = training_images  / 255, testing_images / 255
 
 # assigning labels as the info is numbers, these are the things the neural network can/will  identify
 class_names = ["Plane", "Car", "Bird", "Cat", "Deer", "Dog", "Frog", "Horse","Ship", "Truck" ]
