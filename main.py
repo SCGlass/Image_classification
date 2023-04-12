83dbe5f04c5d09ec6bcb4576a4660782d9c1e309
Sam Glass <sam.charles.glass@gmail.com>
2023-04-12 16:40:49 +0200

    finished with the tutorial.

--- main.py
+++ main.py
@@ -11,28 +11,18 @@ training_images, testing_images = training_images  / 255, testing_images / 255
 # assigning labels as the info is numbers, these are the things the neural network can/will  identify
 class_names = ["Plane", "Car", "Bird", "Cat", "Deer", "Dog", "Frog", "Horse","Ship", "Truck" ]
 
-# now we will create a class which will display 16 images from the dataset
-for i in range(16):
-    plt.subplot(4,4, i +1) # 4x4 grid and each i + 1 will iterate over the images
-    plt.xticks([]) # takes away coordinate system
-    plt.yticks([])
-    plt.imshow(training_images[i], cmap=plt.cm.binary)
-    plt.xlabel(class_names[training_labels[i][0]]) # This will assign the labels depending on the number and the position in the list
-
-plt.show()
-# loading the saved model
-
 training_images = training_images[:20000] # using up to 20000
 training_labels = training_labels[:20000] # using up to 20000
 testing_images = testing_images[:4000] # reducing the testing as well
 testing_labels = testing_labels [:4000]
 
+# loading the saved model
 model = models.load_model("image_classifier.model")
 
 #loading in the picture
-img = cv.imread("horse.jpg")
+img = cv.imread("deer.jpg")
 # convert the color scheme
-img = cv.cvtColor(img, cv.COLOR_BG2RGBA)
+img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
 # shows the image
 plt.imshow(img, cmap=plt.cm.binary)
 # doing the prediction
@@ -40,6 +30,4 @@ prediction = model.predict(np.array([img]) / 255)
 index = np.argmax(prediction) # gives us the neuron with highest activation
 print(f"Prediction is {class_names[index]}")
 
-
-
-
+plt.show()
